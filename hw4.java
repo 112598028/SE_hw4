682a92a92c22d5c829c8d175a740fd9ac8623e45
112598028 <t112598028@ntut.org.tw>
2024-01-03 18:17:21 +0800

    add main function

--- hw4.java
+++ hw4.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class hw4 {
    public static char letterGrade(int score){
             char grade;
@@ -21,4 +25,20 @@ public class hw4 {
             }
             return grade;
         }
+
+    public static void main(String[] args) {
+        int score=0;
+        System.out.print("Enetr the score = ");
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            score = Integer.parseInt(br.readLine());
+            char grade = hw4.letterGrade(score);
+            System.out.println("The grade of " + score + " is " + grade);
+        } catch (NumberFormatException ex){
+            System.out.println("Not an integer!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }       
+    }
 }
